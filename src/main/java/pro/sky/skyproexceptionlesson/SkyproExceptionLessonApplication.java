a4e0c83aedf9032d75c8ef8ecf3639f0ede475b2
Андрей <andrews.ru83@gmail.com>
2022-06-22 10:02:55 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyproexceptionlesson/SkyproExceptionLessonApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproexceptionlesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproExceptionLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproExceptionLessonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyproexceptionlesson/SkyproExceptionLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproexceptionlesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproExceptionLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
